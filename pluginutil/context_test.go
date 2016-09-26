8b0c124824b4271fdf18a6501001bc693b9a42e4
Zac Bergquist <zbergquist99@gmail.com>
2016-09-26 11:16:17 -0600

    [#130754707] remove LoadResourceFromContext
    This function (used by omg-product-bundle) uses the cli.Context
    in its public interface, making it difficult to use from a library
    as the omg-product-bundle's vendored type differs from
    pluginlib's vendored type.
    
    This utility function will be moved into omg-product-bundle.

--- pluginutil/context_test.go
+++ pluginutil/context_test.go
@@ -1,9 +1,6 @@
 package pluginutil_test
 
 import (
-	"flag"
-	"io/ioutil"
-
 	. "github.com/enaml-ops/pluginlib/pluginutil"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -17,42 +14,6 @@ var _ = Describe("NewContext function", func() {
 				&cli.StringFlag{Name: "this"},
 			})
 			Ω(ctx.String("this")).Should(Equal("that"))
-		})
-	})
-})
-
-var _ = Describe("LoadResourceFromContext function", func() {
-	Context("when called with a filename (prefixed by @)", func() {
-		var ctx *cli.Context
-
-		BeforeEach(func() {
-			set := flag.NewFlagSet("test", 0)
-			set.String("my-flag", "@fixtures/foo.txt", "")
-			ctx = cli.NewContext(nil, set, nil)
-		})
-
-		It("reads from the specified file", func() {
-			value, err := LoadResourceFromContext(ctx, "my-flag")
-			Ω(err).Should(BeNil())
-
-			exp, _ := ioutil.ReadFile("fixtures/foo.txt")
-			Ω(value).Should(Equal(string(exp)))
-		})
-	})
-
-	Context("when called with a standard string argument", func() {
-		var ctx *cli.Context
-
-		BeforeEach(func() {
-			set := flag.NewFlagSet("test", 0)
-			set.String("my-flag", "fixtures/deployment_task.json", "")
-			ctx = cli.NewContext(nil, set, nil)
-		})
-
-		It("returns the argument value directly", func() {
-			value, err := LoadResourceFromContext(ctx, "my-flag")
-			Ω(err).Should(BeNil())
-			Ω(value).Should(Equal("fixtures/deployment_task.json"))
 		})
 	})
 })
