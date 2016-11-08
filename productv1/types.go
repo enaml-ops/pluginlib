0256ad3da046638b21db6d367d9dfa77a14e8a14
Zac Bergquist <zbergquist99@gmail.com>
2016-11-08 09:24:07 -0700

    [#133874757] add missing product deployer funcs

--- productv1/types.go
+++ productv1/types.go
@@ -2,10 +2,22 @@
 package product
 
 import (
+	"github.com/enaml-ops/enaml"
 	"github.com/enaml-ops/pluginlib/cred"
+	"github.com/enaml-ops/pluginlib/pcli"
 )
+
+// Meta is the metadata for a product plugin.
+type Meta struct {
+	Name       string
+	Properties map[string]interface{}
+	Releases   []enaml.Release
+	Stemcell   enaml.Stemcell
+}
 
 // ProductDeployer is the interface implemented by V1 product plugins.
 type ProductDeployer interface {
 	GetProduct(args []string, cloudConfig []byte, cs cred.Store) ([]byte, error)
+	GetMeta() Meta
+	GetFlags() []pcli.Flag
 }
