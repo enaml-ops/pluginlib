eac779606d4396dd8f17891305edfa0433d3d025
Anwar Chirakkattil <sadique.anwar@gmail.com>
2016-11-09 09:20:02 -0600

    [#133874765] Added missing Run method

--- cloudconfigv1/plugin.go
+++ cloudconfigv1/plugin.go
@@ -2,6 +2,7 @@ package cloudconfig
 
 import (
 	"net/rpc"
+	"os"
 
 	plugin "github.com/hashicorp/go-plugin"
 )
@@ -22,4 +23,28 @@ func (s Plugin) Server(*plugin.MuxBroker) (interface{}, error) {
 
 func (s Plugin) Client(b *plugin.MuxBroker, c *rpc.Client) (interface{}, error) {
 	return &RPC{client: c}, nil
+}
+
+// PluginsMapHash is an identifier for plugins registered with the go-plugin library.
+const PluginsMapHash = "cloudconfig"
+
+// HandshakeConfig is the configuration for establishing communication between the CLI plugins.
+var HandshakeConfig = plugin.HandshakeConfig{
+	ProtocolVersion:  1,
+	MagicCookieKey:   "BASIC_PLUGIN",
+	MagicCookieValue: "hello",
+}
+
+// Run runs a CloudConfigDeployer as an RPC server.
+// It should be called from a plugin's func main.
+func Run(cc CloudConfigDeployer) {
+	if len(os.Args) >= 2 && os.Args[1] != "" {
+		plugin.Serve(&plugin.ServeConfig{
+			HandshakeConfig: HandshakeConfig,
+			Plugins: map[string]plugin.Plugin{
+				PluginsMapHash: NewCloudConfigPlugin(cc),
+			},
+		})
+		return
+	}
 }
